59d27df700932aa4b661502c6ebf3e3f65150d80
José Vitor <jose-vitor-7@hotmail.com>
2024-10-02 02:24:04 -0300

    feat: improve responsive, add edit button, change checkbox position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import { cn } from '@/lib/utils'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { ShoppingCart } from 'lucide-react'
+import { Pen, ShoppingCart } from 'lucide-react'
 import { NextPage } from 'next'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { v4 as uuidv4 } from 'uuid'
 import { z } from 'zod'
@@ -44,7 +44,7 @@ const data: Groceries = [
   },
   {
     id: uuidv4(),
-    name: 'Item 2',
+    name: 'Item 3',
     price: 5.25,
     quantity: 4,
     caught: true,
@@ -76,6 +76,18 @@ const Home: NextPage = () => {
   const [groceries, setGroceries] = useState<Groceries>([])
 
   const [isCreatingItem, setIsCreatingItem] = useState(false)
+
+  const caughtItems = useMemo(
+    () => groceries.filter((item) => item.caught) ?? [],
+    [groceries],
+  )
+  const total = useMemo(
+    () =>
+      caughtItems
+        .reduce((total, item) => total + item.price * item.quantity, 0)
+        .toFixed(2) ?? 0,
+    [caughtItems],
+  )
 
   const loadGroceries = useCallback(async () => {
     try {
@@ -142,7 +154,16 @@ const Home: NextPage = () => {
         <ThemeToggler />
       </nav>
       <main className="mx-auto max-w-[1200px] w-full px-4 mt-10">
-        <h2 className="text-xl font-bold">Grocery list</h2>
+        <div className="flex justify-between items-center gap-4 flex-col md:flex-row">
+          <h2 className="text-xl font-bold text-center">Lista de compras</h2>
+          <div>
+            <p className="text-center">
+              {caughtItems.length} de {groceries.length} itens pegos, total de
+              R$
+              {total}
+            </p>
+          </div>
+        </div>
         <div className="flex flex-col gap-2 mt-6">
           {!isCreatingItem && (
             <Button
@@ -155,7 +176,7 @@ const Home: NextPage = () => {
           )}
           {isCreatingItem && (
             <form
-              className="flex items-center justify-between gap-2 p-2 border rounded border-dashed"
+              className="flex items-center justify-between gap-2 p-2 border rounded border-dashed flex-wrap md:flex-nowrap"
               onSubmit={handleSubmit(createItem)}
             >
               <Input
@@ -163,45 +184,69 @@ const Home: NextPage = () => {
                 className="border-none"
                 {...register('name')}
               />
-              <Button type="submit" className="">
-                Adicionar
-              </Button>
-              <Button
-                type="reset"
-                variant="secondary"
-                className=""
-                onClick={handleCancelCreateItem}
-              >
-                Cancelar
-              </Button>
+              <div className="flex items-center gap-2 flex-1">
+                <Button type="submit" className="flex-1">
+                  Adicionar
+                </Button>
+                <Button
+                  type="reset"
+                  variant="secondary"
+                  className="flex-1"
+                  onClick={handleCancelCreateItem}
+                >
+                  Cancelar
+                </Button>
+              </div>
             </form>
           )}
           {groceries.map((item) => (
             <div
               key={item.id}
               className={cn(
-                'flex items-center justify-between gap-2 p-4 border rounded',
-                item.caught && 'text-emerald-500',
+                'flex items-center justify-between gap-2 p-4 border rounded flex-col md:flex-row',
+                item.caught &&
+                  item.price > 0 &&
+                  item.quantity > 0 &&
+                  'text-emerald-500',
+                item.caught &&
+                  (item.price === 0 || item.quantity === 0) &&
+                  'text-amber-500',
               )}
             >
-              <p className={cn(item.caught && 'line-through')}>{item.name}</p>
-              <div className="flex items-center gap-8 justify-between max-w-[300px] w-full">
-                <div className="flex items-center gap-4 flex-1">
-                  <span className="text-xs flex-1">
-                    R$ {item.price.toFixed(2)}
-                  </span>
-                  <span className="text-xs flex-1 text-muted-foreground">
-                    {item.quantity} unidades
-                  </span>
-                </div>
+              <div className="flex items-center gap-4 flex-1">
                 <Checkbox
+                  id={item.id}
                   className={cn(
                     item.caught &&
+                      item.price > 0 &&
+                      item.quantity > 0 &&
                       'data-[state=checked]:bg-emerald-500 data-[state=checked]:border-emerald-500',
+                    item.caught &&
+                      item.price === 0 &&
+                      'data-[state=checked]:bg-amber-500 data-[state=checked]:border-amber-500',
                   )}
                   defaultChecked={item.caught}
                   onClick={() => handleUpdateItem(item.id)}
                 />
+                <label
+                  htmlFor={item.id}
+                  className={cn('font-semibold', item.caught && 'line-through')}
+                >
+                  {item.name}
+                </label>
+              </div>
+              <div className="flex items-center gap-8 justify-between max-w-[300px] w-full">
+                <div className="flex items-center gap-4 flex-1 flex-wrap text-center md:text-left">
+                  <span className="text-xs flex-1">
+                    R$ {item.price.toFixed(2)}
+                  </span>
+                  <span className="text-xs flex-1">
+                    {item.quantity} unidades
+                  </span>
+                </div>
+                <Button variant="outline" size="icon">
+                  <Pen className="size-4" />
+                </Button>
               </div>
             </div>
           ))}
